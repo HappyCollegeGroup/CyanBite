6d3ab6ee452d3ddd0d2af1a513a953d1ab03cdde
Kenline <kenline0422@gmail.com>
2025-05-08 00:24:44 +0800

    manage and add restaurant handling

---
+++ frontend/app/src/main/java/fcu/app/cyanbite/ui/RestaurantManageMenuFragment.java
@@ -0,0 +1,66 @@
+package fcu.app.cyanbite.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fcu.app.cyanbite.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RestaurantManageMenuFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RestaurantManageMenuFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public RestaurantManageMenuFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RestaurantManageMenuFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static RestaurantManageMenuFragment newInstance(String param1, String param2) {
+        RestaurantManageMenuFragment fragment = new RestaurantManageMenuFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_restaurant_manage_menu, container, false);
+    }
+}
